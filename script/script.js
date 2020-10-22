2d0bbcdd114f0f98d13605ce0ca04eec449b6679
jiyoonm <moonj066@newschool.edu>
2020-10-21 21:37:15 -0500

    new table

--- script/script.js
+++ script/script.js
@@ -4,6 +4,7 @@ var data2 = [];
 
 var Airtable = require('airtable');
 
+
 function findData() {
 
   var base = new Airtable({apiKey: 'keyJvXFNWQIMm9EA2'}).base('appmrMX9i7Il9Ewtq');
@@ -19,10 +20,11 @@ function findData() {
     function done(err) {
       if (err) { console.error(err); return; 
     }
-      ready();
+
   });}
 
-findData();
+
+  findData();
 
 function findData2() {
 
@@ -31,7 +33,7 @@ function findData2() {
       view: "Grid view",
     }).eachPage(function page(records, fetchNextPage) {
         records.forEach(function(record) {
-          data2.push({name1: record.fields.recordID, location:record.fields.Location });
+          data2.push({name1: record.fields.dd, n: record.fields.Neighborhood2, location:record.fields.Location });
         });
 
         fetchNextPage();
@@ -39,9 +41,11 @@ function findData2() {
       if (err) { console.error(err); 
         return; 
     }
+    ready(data1,data2);
   });
 
 }
+findData2();
 //button data switch
 function dataSwap(datasetGroup) {  
   d3.selectAll('circle')
@@ -63,16 +67,15 @@ function dataSwap(datasetGroup) {
               return 0
               }
             ;})
-  d3.select('#titleText')
-    .text('Wifi ' + datasetGroup);
+
   };
 
-const margin = {top: 20, right: 30, bottom: 20, left: 30};
+const margin = {top: 20, right: 30, bottom: 50, left: 70};
 const outerWidth = 700;
 const outerHeight = 600;
 const innerWidth = outerWidth - margin.left - margin.right
 const innerHeight = outerHeight - margin.top - margin.bottom
-const radius=5;
+const radius=6;
 const transitionTime=1000;
 //SVG 
 
@@ -100,19 +103,23 @@ const xAxisGroup = svg.append("g")
 const yAxisGroup = svg.append("g")
   .attr("class", "y axis") 
   .call(yAxis);
-  var color = d3.scaleOrdinal()
+var color = d3.scaleOrdinal()
   .domain(["Manhattan", "Bronx", "Brooklyn", "Queens","Staten Island" ])
-  .range([ "#440154ff", "#21908dff", "#fde725ff", "red", "blue"])
+  .range([ "#EFB605", "#E01A25", "#991C71","#2074A0",  "#7EB852"])
 
-function ready() {
+function ready(data1, data2) {
     const startData =  data1
     const boroughList = d3.set(data1.map(function(d) { return d.borough })).values();
-    const neighborhoodList = d3.set(data1.map(function(d) { return d.name })).values();
-   
+    const neighborhoodList = d3.set(data2.map(function(D) { return D.n })).values();
+ 
     d3.select('#buttonsDiv')
       .selectAll('button')
       .data(boroughList)
       .enter().append('button')
+      .attr('class','button')
+      .style("background-color",function (d) { return color(d) }  )
+      .style('opacity', .9)
+    .style('color','white')
       .text(function(d) { return d; })
       .on('click', function(d) {
          dataSwap(d,data1) 
@@ -147,37 +154,51 @@ function ready() {
             .duration(transitionTime) 
             .attr('r', radius)
             d3.selectAll('circle')
-            .style('opacity', 1)
+            .style('opacity', .8)
         });
 
     //Data points for neighborhoods
     wifiGroup.append('circle')
-        .style('fill', '#a4d7ed')
+        .style('opacity', .8)
         .attr('class', 'wifiCircle')
         .attr('r', radius)
         .style("fill", function (d) { return color(d.borough) } )
         .on("mouseover", tipMouseover)
+        .on("click", tip)
         .on("mouseout", tipMouseout)
     //Data title
-    d3.select('#titleDiv')
-        .append('h1')
-        .attr('id', 'titleText')
-        .text('Wifi Access in NYC')
+   
+    svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x",0 - (innerHeight / 2))
+    .attr("dy", "1em")
+    .style("color",'#6e6e6e')
+    .style('font-size','17px')
+    .style("text-anchor", "middle")
+    .style("color","#6e6e6e")
+    .text("# of Free Wi-Fi Hotspot Locations"); 
+ 
+    svg.append("text")
+    .attr("class", "x label")
+  
+    .style("text-anchor", "middle")
+    .attr("x", innerWidth/2)
+    .style('font-size','17px')
+    .attr("y", innerHeight + 40)
+    .style("color","#6e6e6e")
+
+    .text("% of Households without Internet");
         
   }
 
   var tooltip = d3.select("#my_dataviz").append("div")
   .attr("class", "tooltip")
   .style("opacity", 0);
-  var tipMouseover = function(d) {    
-    // for (i = 0; i < data2.length; i++) {
-    //   if (data2.name[i]===d.id[i]){
-    //     console.log(data2.name[i])
-    //   }
-    // }
-    const result = data2.filter(word => data2.name === d.id);
 
-console.log(result);
+  var tipMouseover = function(d) {    
+
+
     var html  = "<span style='color:" +  color(d.borough)  + ";'>" + d.name + "</span><br/>";
     tooltip.html(html)
         .style("left", (d3.event.pageX + 15) + "px")
@@ -192,4 +213,113 @@ console.log(result);
     tooltip.transition()
         .duration(transitionTime/2)
         .style("opacity", 0);
+   
   };
+
+  var locations=[];
+
+
+  var tip = function(d) {
+
+    locations=[];
+
+    // var 
+    for (i = 0; i < data2.length; i++) {
+      if (d.name===data2[i].n){
+        locations.push({name1: data2[i].location, name2:data2[i].name1 });
+  }
+  
+    }
+    console.log( locations)
+    update(locations, ['name1','name2']);
+
+    var x = document.getElementById("table1");
+    var y = document.getElementById("dissapear");
+        if (x.style.display === "none" && y.style.display === "none" ) {
+      x.style.display = "inline-block";
+      y.style.display = "block";
+      y.innerHTML = ("Free Wi-fi Hotspot Locations in " + d.name)
+    } else {
+      x.style.display = "none";
+      y.style.display = "none";
+    }
+  }
+  
+var table = d3.select('table')
+
+var ident = function(d) { return d.value; };
+var update = function(locations, columns) {
+
+
+  var rows = table.selectAll('tr').data(locations)
+   //////////////////////////////////////////
+  // ROW UPDATE SELECTION
+
+  // Update cells in existing rows.
+
+  var cells = rows.selectAll("td")
+  .data(function(row) {
+      return columns.map(function(column) {
+          return {column: column, value: row[column]};
+      });
+  });
+
+  cells.attr('class', 'update');
+  
+  // Cells enter selection
+  cells.enter().append('td')
+    .style('opacity', 0.0)
+    .attr('class', 'enter')
+    .transition()
+    .delay(200)
+    .duration(500)
+    .style('opacity', 1.0);
+
+  cells.text(ident);
+
+  // Cells exit selection
+  cells.exit()
+    .attr('class', 'exit')
+    .transition()
+    .delay(200)
+    .duration(500)
+    .style('opacity', 0.0)
+    .remove();
+
+  //////////////////////////////////////////
+  // ROW ENTER SELECTION
+  // Add new rows
+  var cells_in_new_rows = rows.enter().append('tr')
+                              .selectAll('td')
+                              .data(function(row) {
+                                return columns.map(function(column) {
+                                    return {column: column, value: row[column]};
+                                });
+                            });
+
+  cells_in_new_rows.enter().append('td')
+    .style('opacity', 0.0)
+    .attr('class', 'enter')
+    .transition()
+      .delay(200)
+      .duration(500)
+    .style('opacity', 1.0);
+
+  cells_in_new_rows.text(ident);
+
+  /////////////////////////////////////////
+  // ROW EXIT SELECTION
+  // Remove old rows
+  rows.exit()
+    .attr('class', 'exit')
+    .transition()
+    .delay(200)
+    .duration(500)
+
+    .style('opacity', 0.0)
+    .remove();
+
+  table.selectAll('tr').select('td').classed('row-header', true);
+
+
+};
